08feb5b3409eb5d80b26745d13055667a5d47e7f
kartiknawal <kartiknawal.7@gmail.com>
2020-10-05 14:32:59 +0530

    [kartiknawal] add.creating the board

---
+++ TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        public void UC1_CreateBoard()
+        {
+            char[] board = new char[10];
+            int i;
+            for (i = 1; i <= 9; i++)
+            {
+                board[i] = ' ';
+
+            }
+        }
+    }
+}
