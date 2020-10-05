1aa95f8a8bb3cd93def4077a58438162b2c2c25d
kartiknawal <kartiknawal.7@gmail.com>
2020-10-05 15:56:48 +0530

    [kartiknawal] add.show board

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -6,9 +6,10 @@ namespace TicTacToe
 {
     class TicTacToeGame
     {
+        char[] board = new char[10];
         public void CreateBoard()
         {
-            char[] board = new char[10];
+            
             int i;
             for (i = 1; i <= 9; i++)
             {
@@ -22,6 +23,19 @@ namespace TicTacToe
             Console.WriteLine("Choose between X or O : ");
             char letter = Convert.ToChar(Console.ReadLine());
             return letter;
+        }
+
+        public void showBoard()
+        {
+            int x = 1;
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine(board[x] + " ");
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
